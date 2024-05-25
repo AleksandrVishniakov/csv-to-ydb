96f5556c690627d0388750e3cb3f688629ce9cf3
Dmitriy <burindima324@gmail.com>
2024-05-25 17:46:59 +0300

    add insert with bulk

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package csv_to_ydb
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -137,4 +138,40 @@ func TestInsertData(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+func TestInsertDataWithBulk(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	t.Cleanup(cancel)
+
+	fmt.Println("connecting...")
+
+	db, err := NewYDB(ctx, "grpc://localhost:2136/local")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		_ = db.Shutdown(ctx)
+	})
+
+	fmt.Println("db connected")
+
+	repo := NewRepository(db.Driver)
+	const tableName = "user1/table1"
+	dataCh := make(chan []string)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err = repo.InsertDataWithBulk(ctx, tableName, dataCh)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+	go func() {
+		defer close(dataCh)
+		dataCh <- []string{"col1_row1", "col2_row1", "col3_row1"}
+		dataCh <- []string{"col1_row2", "col2_row2", "col3_row2"}
+		dataCh <- []string{"col1_row3", "col2_row3", "col3_row3"}
+	}()
+	wg.Wait()
 }
